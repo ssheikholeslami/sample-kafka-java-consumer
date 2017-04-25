72caab0a868f8974ac9f29c8f18535fa2ca5f46d
Sina Sheikholeslami <ssheikholeslami@gmail.com>
2017-04-25 15:25:33 +0430

    Offset Commits

--- src/main/java/ClientRunner.java
+++ src/main/java/ClientRunner.java
@@ -1,11 +1,11 @@
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -18,7 +18,7 @@ public class ClientRunner {
 
         HashMap<String, Integer> custCountryMap = new HashMap<String, Integer>();
 
-        Logger log = Logger.getLogger(ClientRunner.class.getName());
+        final Logger log = Logger.getLogger(ClientRunner.class.getName());
 
         Properties kafkaProps = new Properties();
 
@@ -37,6 +37,7 @@ public class ClientRunner {
 
         // The Poll loop
         try {
+
             while (true) {
 
                 ConsumerRecords<String, String> records = consumer.poll(100); // keep polling or you'll be considered DEAD
@@ -44,17 +45,46 @@ public class ClientRunner {
                     log.debug("topic = " + record.topic() + ", partition = " + record.partition() + ", offset = " + record.offset() + ", customer = " + record.key() + ", country = " + record.value());
 
                     int updatedCount = 1;
-                    if (custCountryMap.containsValue(record.value())){
+                    if (custCountryMap.containsValue(record.value())) {
                         updatedCount = custCountryMap.get(record.value()) + 1;
                     }
                     custCountryMap.put(record.value(), updatedCount);
                     JSONObject json = new JSONObject(custCountryMap);
                     System.out.println(json.toString());
                 }
+                if (args[1].equalsIgnoreCase("sync")) {
+
+                    try {
+                        consumer.commitSync(); //This is a synchronous commits and will block until either the commit succeeds or an unrecoverable error is encountered
+                    } catch (CommitFailedException e) {
+                        log.error("Commit Failed, " + e);
+                    }
+                } else if (args[1].equalsIgnoreCase("async")) {
+                    try {
+                        consumer.commitAsync();
+                    } catch (CommitFailedException e) {
+                        log.error("Commit Failed, " + e);
+                    }
+                } else if (args[1].equalsIgnoreCase("async-callback")) {
+                    consumer.commitAsync(new OffsetCommitCallback() {
+                        public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception exception) {
+                            if (exception != null) {
+
+                                log.error("Commit Failed for Offsets {" + offsets + "}" + exception);
+
+                            }
+                        }
+                    });
+                }
+
             }
-        }
-        finally {
-            consumer.close();
+
+        } finally {
+            try {
+                consumer.commitSync(); //make sure the commits are received by the broker before shutdown
+            } finally {
+                consumer.close();
+            }
         }
 
     }
